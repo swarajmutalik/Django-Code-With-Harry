00c3a6f94df3e87b01ed9182c68a9bd29831065e
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-06-28 19:12:57 +0530

    Update settings.py

--- MyCart/MyCart/settings.py
+++ MyCart/MyCart/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MyCart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['MyCart/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
