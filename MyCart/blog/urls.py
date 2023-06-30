a6b8218ae1a6907244d144d0d58941066c34456f
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-06-30 22:03:08 +0530

    Update urls.py

--- MyCart/blog/urls.py
+++ MyCart/blog/urls.py
@@ -1,7 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
 from . import views
-from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="ShopHome")
+    path('', views.index, name="BlogHome"),
+    path('blogpost/', views.blogpost, name="blogPost"),
 ]
-
