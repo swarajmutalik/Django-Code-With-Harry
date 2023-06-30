dc744c872e7612333341ab8c1d4ddeeb89934d4a
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-06-30 22:02:49 +0530

    Update views.py

--- MyCart/blog/views.py
+++ MyCart/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-# Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, "blog/index.html")
+
+def blogpost(request):
+    return render(request, "blog/blogpost.html")
