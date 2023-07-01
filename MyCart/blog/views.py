62cbe25f3b8707f990fe1be97ac45efeeb98e151
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-07-01 20:53:50 +0530

    Update views.py

--- MyCart/blog/views.py
+++ MyCart/blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Blogpost
+# Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, "blog/index.html")
 
-def blogpost(request):
-    return render(request, "blog/blogpost.html")
+def index(request):
+    myposts = Blogpost.objects.all()
+    print(myposts)
+    return render(request, 'blog/index.html', {'myposts': myposts})
+
+
+def blogpost(request, id):
+    post = Blogpost.objects.filter(post_id=id)[0]
+    print(post)
+    return render(request, 'blog/blogpost.html',
+                  {'post': post})
