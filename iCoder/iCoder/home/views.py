fd08cc72594f113de60f25988660a796154f7904
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-07-05 21:29:44 +0530

    Update views.py

--- iCoder/iCoder/home/views.py
+++ iCoder/iCoder/home/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
-def home(request): 
-    return HttpResponse('This is home ')
+def home(request):
+    return render(request, 'home/home.html')
+
 
 def contact(request):
-    return HttpResponse("This is contact")
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        contact = Contact(name=name, email=email, phone=phone, content=content)
+        contact.save()
+    return render(request, "home/contact.html")
 
-def about(request): 
-    return HttpResponse('This is about')
+
+def about(request):
+    return render(request, 'home/about.html')
