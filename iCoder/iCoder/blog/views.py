a4e544b2cf0fb0f37e76177331f9b5b1fa8af3cb
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-07-06 12:54:43 +0530

    Update views.py

--- iCoder/iCoder/blog/views.py
+++ iCoder/iCoder/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import Post
+
 
 # Create your views here.
-def blogHome(request): 
-    return HttpResponse('This is  blog home. We will keep all blog posts here')
+def blogHome(request):
+    allPosts = Post.objects.all()
+    context = {'allPosts': allPosts}
+    return render(request, "blog/blogHome.html", context)
 
-def blogPost(request, slug): 
-    return HttpResponse(f'This is blogPost : {slug}')
+
+def blogPost(request, slug):
+    return render(request, 'blog/blogPost.html')
