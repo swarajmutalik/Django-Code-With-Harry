25b0c1c25945b0ecb2e74edd1862b3628bc07307
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-07-07 21:38:25 +0530

    Update views.py

--- iCoder/iCoder/home/views.py
+++ iCoder/iCoder/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from home.models import Contact
 from django.contrib import messages
+from blog.models import Post
 
 
 # Create your views here.
@@ -26,3 +27,18 @@ def contact(request):
 
 def about(request):
     return render(request, 'home/about.html')
+
+
+def search(request):
+    query = request.GET['query']
+    if len(query) > 78:
+        allPosts = Post.objects.none()
+    else:
+        allPostsTitle = Post.objects.filter(title__icontains=query)
+        allPostsAuthor = Post.objects.filter(author__icontains=query)
+        allPostsContent = Post.objects.filter(content__icontains=query)
+        allPosts = allPostsTitle.union(allPostsContent, allPostsAuthor)
+    if allPosts.count() == 0:
+        messages.warning(request, "No search results found. Please refine your query.")
+    params = {'allPosts': allPosts, 'query': query}
+    return render(request, 'home/search.html', params)
