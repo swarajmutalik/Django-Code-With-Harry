791035a1a738ba8aa85cfe7986e61d9d902cb22e
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-07-12 19:55:35 +0530

    Update views.py

--- iCoder/iCoder/blog/views.py
+++ iCoder/iCoder/blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
-from blog.models import Post
+from django.shortcuts import render, HttpResponse, redirect
+from blog.models import Post, BlogComment
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -10,4 +12,20 @@ def blogHome(request):
 
 
 def blogPost(request, slug):
-    return render(request, 'blog/blogPost.html')
+    post = Post.objects.filter(slug=slug).first()
+    comments = BlogComment.objects.filter(post=post)
+    context = {'post': post, 'comments': comments}
+    return render(request, "blog/blogPost.html", context)
+
+
+def postComment(request):
+    if request.method == "POST":
+        comment = request.POST.get('comment')
+        user = request.user
+        postSno = request.POST.get('postSno')
+        post = Post.objects.get(sno=postSno)
+        comment = BlogComment(comment=comment, user=user, post=post)
+        comment.save()
+        messages.success(request, "Your comment has been posted successfully")
+
+    return redirect(f"/blog/{post.slug}")
