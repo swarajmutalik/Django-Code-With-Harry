86944d36a5b774ee717d83d401cdb6a6d6d9a4b0
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2023-07-13 14:34:29 +0530

    Update urls.py

--- iCoder/iCoder/iCoder/urls.py
+++ iCoder/iCoder/iCoder/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+
+admin.site.site_header = "iCoder Admin"
+admin.site.site_title = "iCoder Admin Panel"
+admin.site.index_title = "Welcome to iCoder Admin Panel"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls')),
-    path('',include('blog.urls')),
+    path('', include('home.urls')),
+    path('blog/', include('blog.urls')),
 
 ]
+
+
